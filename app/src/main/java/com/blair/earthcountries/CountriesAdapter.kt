3a06f103b5c8bebcfba2b0e81d54866d0087b067
blairMe <felixanachoni@gmail.com>
2022-11-17 22:05:42 +0300

    List of countries

--- app/src/main/java/com/blair/earthcountries/CountriesAdapter.kt
+++ app/src/main/java/com/blair/earthcountries/CountriesAdapter.kt
@@ -1,13 +1,16 @@
 package com.blair.earthcountries
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.blair.earthcountries.databinding.CountryItemBinding
 
 class CountriesAdapter : RecyclerView.Adapter<CountriesAdapter.CountriesViewHolder>() {
-    inner class CountriesViewHolder(binding: CountryItemBinding) : RecyclerView.ViewHolder(binding.root)
+    inner class CountriesViewHolder(val binding: CountryItemBinding) : RecyclerView.ViewHolder(binding.root)
 
-    private val diffCallBack = object : DiffUtil.ItemCallback<Countries> {
+    private val diffCallBack = object : DiffUtil.ItemCallback<Countries>() {
         override fun areItemsTheSame(oldItem: Countries, newItem: Countries): Boolean {
             return oldItem.name == newItem.name
         }
@@ -15,6 +18,26 @@ class CountriesAdapter : RecyclerView.Adapter<CountriesAdapter.CountriesViewHold
         override fun areContentsTheSame(oldItem: Countries, newItem: Countries): Boolean {
             return oldItem == newItem
         }
+    }
 
+    private val differ = AsyncListDiffer(this, diffCallBack)
+    var countries : List<Countries>
+        get() = differ.currentList
+        set(value) {differ.submitList(value)}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountriesViewHolder {
+        return CountriesViewHolder(CountryItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        ))
+    }
+
+    override fun onBindViewHolder(holder: CountriesViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        return countries.size
     }
 }
